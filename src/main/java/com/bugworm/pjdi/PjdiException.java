2c03792cf26c90e13d7ad34f0d16e7ff569168d4
fukaiyas <yasuyuki.fukai@gmail.com>
2016-01-03 10:30:44 +0900

    ざっとコメントを記述

--- src/main/java/com/bugworm/pjdi/PjdiException.java
+++ src/main/java/com/bugworm/pjdi/PjdiException.java
@@ -1,9 +1,23 @@
 package com.bugworm.pjdi;
 
+/**
+ * An exception class for PJDI project.
+ */
 public class PjdiException extends RuntimeException{
+
+    /**
+     * Constructs a PjdiException with the specified detail message.
+     * @param message The detail message
+     */
     public PjdiException(String message){
         super(message);
     }
+
+    /**
+     * Constructs a PjdiException with the specified detail message and cause.
+     * @param message The detail message
+     * @param throwable The cause
+     */
     public PjdiException(String message, Throwable throwable){
         super(message, throwable);
     }
--- src/main/java/com/bugworm/pjdi/WrapperBuilder.java
+++ src/main/java/com/bugworm/pjdi/WrapperBuilder.java
@@ -9,14 +9,36 @@ import java.util.Optional;
  */
 public abstract class WrapperBuilder<T extends WrapperBuilder<T>>{
 
+    /** VirtualMachineManager object */
     protected final VirtualMachineManager manager;
 
+    /** Whether to suspend when target VM started */
     protected Optional<Boolean> suspendMode = Optional.empty();
 
+    /** Name for selecting Connector */
+    protected Optional<String> name = Optional.empty();
+
+    /**
+     * Factory method for ListeningConnectorWrapperBuilder with default VirtualMachineManager object.
+     * @return ListeningConnectorWrapperBuilder object
+     */
+    public static ListeningConnectorWrapperBuilder withListeningConnector(){
+        return new ListeningConnectorWrapperBuilder();
+    }
+
+    /**
+     * Constructs a WrapperBuilder with the specified VirtualMachineManager object.
+     * @param vmm VirtualMachineManager object
+     */
     public WrapperBuilder(VirtualMachineManager vmm){
         manager = vmm;
     }
 
+    /**
+     * Sets the suspend property.
+     * @param suspend Whether to suspend when target VM started
+     * @return This instance
+     */
     public T suspend(boolean suspend){
         suspendMode = Optional.of(suspend);
         return (T)this;
